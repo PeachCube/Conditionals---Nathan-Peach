c2079fc722aee12b781f8bb92430420e3dc4cfd0
PeachCube <nathanpeach500@live.com>
2023-09-25 08:58:02 -0300

    test

---
+++ Conditionals - Nathan Peach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Conditionals___Nathan_Peach
+{
+    internal class Program
+    {
+        //Variable declaration
+        static int health;
+        static void Main(string[] args)
+        {
+            //Variable initialization
+            health = 100;
+
+            Console.WriteLine("-Conditionals Assignment-");
+            
+            ShowHud();
+            TakeDamage(25);
+            ShowHud();
+            TakeDamage(50);
+            ShowHud();
+            TakeDamage(25);
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+
+        }
+        static void TakeDamage(int damage)
+        {
+            health = health - damage;
+        }
+        static void ShowHud()
+        {
+            Console.WriteLine("Health: " + health);
+        }
+    }
+}
