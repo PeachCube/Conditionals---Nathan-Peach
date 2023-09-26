536b87d76d36fe786ff692ad50f140dc311c272c
PeachCube <nathanpeach500@live.com>
2023-09-26 15:20:59 -0300

    finished version w/ health status and weapon + armor display

--- Conditionals - Nathan Peach/Program.cs
+++ Conditionals - Nathan Peach/Program.cs
@@ -13,29 +13,81 @@ namespace Conditionals___Nathan_Peach
         static int health;
         static string weapon;
         static string armor;
+        static int weaponid = 0;
+        static int armorid = 0;
+        static string HS;
         static void Main(string[] args)
         {
             //Variable initialization
             health = 3;
-            
+
             Console.WriteLine(" - CONDITIONALS ASSIGNMENT - ");
-            
+
             ShowHud();
-            GetWeapon(0);
+            GetWeapon(1);
             ShowHud();
-            GetArmor(0);
+            GetArmor(1);
             ShowHud();
             TakeDamage(1);
             ShowHud();
             TakeDamage(2);
             ShowHud();
-            //new event
+            Heal(1);
             ShowHud();
             //new event
+        }
+        static void Heal(int healing)
+        {
+            health = health + healing;
+            Console.WriteLine("- Player healed for "+healing+" HP.");
+        }
+        static void GetWeapon(int weaponid)
+        {
+            if (weaponid == 1)
+            {
+                weapon = "Broadsword";
+                Console.WriteLine("- You found a " + weapon);
+            }
+        }
+        static void GetArmor(int armorid)
+        {
+            if (armorid == 1)
+            {
+                armor = "Chainmail";
+                Console.WriteLine("- You found a set of " + armor);
+            }
+        }
+        static void TakeDamage(int damage)
+        {
+            if (damage == 1)
+                Console.WriteLine("- Player is about to lose a heart.");
+            if (damage >= 2)
+                Console.WriteLine("- Player is about to lose " + damage + " hearts.");
+
+            if (armor == "Chainmail")
+            {
+                damage = damage - 1;
+                Console.WriteLine("- Your " + armor + " reduced the damage by 1 heart!");
+            }
+
+            //Takes away health by the damage value
+            health = health - damage;
+
+            //sets health to 0 if health is less than or equal to 0
+            if (health <= 0)
+            {
+                health = 0;
+                Console.WriteLine("-  Player has died...");
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to exit game --->");
+                Console.ReadKey(false);
+                Environment.Exit(0);
+            }
         }
         static void ShowHud()
         {
             //Displays a different number of hearts based on user health
+            Console.WriteLine("");  
             Console.WriteLine("");
             Console.WriteLine("[]----------[]");
             if (health == 3)
@@ -51,54 +103,30 @@ namespace Conditionals___Nathan_Peach
                 Console.WriteLine("|| <3       ||");
             }
             Console.WriteLine("[]----------[]");
-            Console.WriteLine("Equipment: "+weapon+" & "+armor);
+            Console.WriteLine("");
+            Console.WriteLine("Weapon: " + weapon);
+            Console.WriteLine("Armor: " + armor);
+            if (health == 3)
+            {
+                HS = "Very Healthy";
+                Console.WriteLine("Health Status: " + HS); 
+            }
+            if (health == 2)
+            {
+                HS = "Slightly Damaged";
+                Console.WriteLine("Health Status: " + HS);
+            }
+            if (health == 1)
+            {
+                HS = "Greatly Wounded";
+                Console.WriteLine("Health Status: " + HS);
+            }
             Console.WriteLine("");
             Console.WriteLine("Press any key to continue --->");
             Console.WriteLine("");
+            Console.WriteLine("V V V EVENT LOG V V V");
+            Console.WriteLine("");
             Console.ReadKey(false);
-        }
-        static void GetWeapon(int weaponid)
-        {
-            if (weaponid == 0)
-            {
-                weapon = "Sword";
-                Console.WriteLine("You found a " + weapon);
-            }
-        }
-        static void GetArmor(int armorid)
-        {
-            if (armorid == 0)
-            {
-                armor = "Chainmail";
-                Console.WriteLine("You found a set of " + armor);
-            }
-        }
-        static void TakeDamage(int damage)
-        {
-            if (damage == 1)
-                Console.WriteLine("Player is about to lose a heart.");
-            if (damage >= 2)
-                Console.WriteLine("Player is about to lose " + damage + " hearts.");
-
-            if (armor == "Chainmail")
-            {
-                damage = damage - 1;
-                Console.WriteLine("The damage was reduced by your " + armor + "!");
-            }
-
-            //Takes away health by the damage value
-            health = health - damage;
-            
-            //sets health to 0 if health is less than or equal to 0
-            if (health <= 0)
-            {
-                health = 0;
-                Console.WriteLine("Player has died...");
-                Console.WriteLine("");
-                Console.WriteLine("Press any key to exit game --->");
-                Console.ReadKey(false);
-                Environment.Exit(0);
-            }
         }
     }
 }
